1fcf6643e2b7caa69493b1c0be1d6b96f1a68205
YashAce <yashwanth.crtr@gmail.com>
2025-05-24 20:12:57 +0530

    Endpoint url mod

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,7 +21,7 @@ const App = () => {
       }
     ]);
     try {
-      const response = await fetch('http://localhost:3001/ping', {
+      const response = await fetch('http://acc-backend-service:3001/ping', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ userId, message })
